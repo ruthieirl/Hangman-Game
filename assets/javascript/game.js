4b69598b817b63b847bdbc4d115de57abbee7d18
ruthieirl <ruthfromjersey@gmail.com>
2017-10-12 15:28:58 -0400

    changing HTML for word selected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,8 +14,30 @@ var guessesLeft = 9;
 //Functions
 //*************************************************************
 function startGame() {
+	
 	selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
 	console.log(selectedWord);
+
+	lettersInWord = selectedWord.split("");
+	console.log(lettersInWord);
+
+	numBlanks = lettersInWord.length;
+	console.log(numBlanks);
+	
+	//Resets
+	guessesLeft = 9;
+	wrongLetters = [];
+	blankAndSuccess = [];
+
+	//Populate blanks
+	for (var i = 0; i < numBlanks; i++) {
+		blankAndSuccess.push("_");
+		console.log(blankAndSuccess);
+	};
+
+	document.getElementById("wordToGuess").innerHTML = blankAndSuccess.join(" ");
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
+
 }
 //Main Process
 startGame();
